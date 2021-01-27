585a40f9d41a4413587712ee110970b2707f0007
Christian Chavez <christiancchavez10@hotmail.com>
2021-01-27 10:11:55 -0500

    add index.js to every components, also install node-sass package previous version

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,10 +1,18 @@
 import React from 'react';
+//Style
+import './header.scss';
+//Image
+import ball from '../../Assets/images/ballHeader.png';
 
 const Header = () => {
     return (
-        <h1>
-            Soccer fan
-        </h1>
+        <div className="header">
+            <h1 className="header__title">
+                Soccer fan
+            </h1>
+            <img src={ball} alt="ball icon" />
+        </div>
+        
     );
 };
 
