582db578888edc7d051da9b49d7928e95d741ffd
Christian Chavez <christiancchavez10@hotmail.com>
2021-01-29 11:20:30 -0500

    add style to TopScore and TopScores components

--- src/Components/TopScore/TopScore.js
+++ src/Components/TopScore/TopScore.js
@@ -1,25 +1,14 @@
 import React from 'react';
+//Style
+import './topScore.scss';
 
 const TopScore = ({ player_name, team_name, goals }) => {
     return (
-        <div>
-            <table>
-                <thead>
-                    <tr>
-                        <th>Player</th>
-                        <th>Team</th>
-                        <th>Goals</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>{player_name}</td>
-                        <td>{team_name}</td>
-                        <td>{goals}</td>
-                    </tr>
-                </tbody>
-            </table>
-        </div>
+        <tr className="topscore">
+            <td className="topscore__player">{player_name}</td>
+            <td className="topscore__team">{team_name}</td>
+            <td className="topscore__goals">{goals}</td>
+        </tr>
     );
 };
 
--- src/Components/TopScores/TopScores.js
+++ src/Components/TopScores/TopScores.js
@@ -3,24 +3,35 @@ import React, { useContext } from 'react';
 import { SoccerFanContext } from '../../context/contextSoccer';
 //Components
 import TopScore from './../TopScore/TopScore';
-//Images 
-import podium from '../../Assets/images/podium.png';
+//Style
+import './topScores.scss';
 
 const TopScores = () => {
 
     const { topScores } = useContext(SoccerFanContext);
 
     return (
-        <div>
-            {topScores.map(topscore =>
-                <TopScore 
-                    key={topscore.player_key} 
-                    player_name={topscore.player_name} 
-                    team_name={topscore.team_name} 
-                    goals={topscore.goals}
-                />    
-            )}
-            <img src={podium} alt=" league's topscore" />
+        <div className="topscores">
+            <h3 className="topscores__title">TopScores</h3>
+            <table className="topscores-table">
+                <thead>
+                    <tr>
+                        <th>Player</th>
+                        <th>Team</th>
+                        <th>Goals</th>
+                    </tr>
+                </thead>
+                <tbody>
+                {topScores.map(topscore =>
+                    <TopScore 
+                        key={topscore.player_key} 
+                        player_name={topscore.player_name} 
+                        team_name={topscore.team_name} 
+                        goals={topscore.goals}
+                    />    
+                )}
+                </tbody>
+            </table>
         </div>
     );
 };
