bab9238523b6684459eec7232e70f4a6887b7778
Christian Chavez <christiancchavez10@hotmail.com>
2021-02-11 09:41:53 -0500

    create one function to do three fetch data to API

--- src/Components/Standings/Standings.js
+++ src/Components/Standings/Standings.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState, useEffect } from 'react';
 //Components
 import Standing from './../Standing/Standing';
 //Context
@@ -8,44 +8,57 @@ import './standings.scss';
 
 const Standings = () => {
 
-    const { standings } = useContext(SoccerFanContext);
+    const { standings, competition } = useContext(SoccerFanContext);
+    const [ renderComponent, setRenderComponent] = useState(true);
+
+    
+    useEffect(() => {
+        return () => {
+            setRenderComponent(false);
+        };
+    }, [competition]);
+
 
     return (
-        <div className="standings">
-            <h3 className="standings__title">Standings</h3>
-            <table className="standings-table">
-                <thead>
-                    <tr>
-                        <th>#</th>
-                        <th>Team</th>
-                        <th>Pts</th>
-                        <th>Ga</th>
-                        <th>W</th>
-                        <th>D</th>
-                        <th>L</th>
-                        <th>GF</th>
-                        <th>GA</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {standings.map(standing => 
-                        <Standing 
-                            key={standing.team_id}
-                            team_place={standing.overall_league_position} 
-                            team_name={standing.team_name} 
-                            team_points={standing.overall_league_PTS}
-                            games_played={standing.overall_league_payed} 
-                            games_win={standing.overall_league_W} 
-                            games_defeat={standing.overall_league_D} 
-                            games_lost={standing.overall_league_L} 
-                            goals_for={standing.overall_league_GF} 
-                            goals_against={standing.overall_league_GA}
-                            team_badge={standing.team_badge} 
-                        />    
-                    )}
-                </tbody>
-            </table>
-        </div>
+        <>
+            {renderComponent &&
+                <div className="standings">
+                    <h3 className="standings__title">Standings</h3>
+                    <table className="standings-table">
+                        <thead>
+                            <tr>
+                                <th>#</th>
+                                <th>Team</th>
+                                <th>Pts</th>
+                                <th>Ga</th>
+                                <th>W</th>
+                                <th>D</th>
+                                <th>L</th>
+                                <th>GF</th>
+                                <th>GA</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {standings.map(standing => 
+                                <Standing 
+                                    key={standing.team_id}
+                                    team_place={standing.overall_league_position} 
+                                    team_name={standing.team_name} 
+                                    team_points={standing.overall_league_PTS}
+                                    games_played={standing.overall_league_payed} 
+                                    games_win={standing.overall_league_W} 
+                                    games_defeat={standing.overall_league_D} 
+                                    games_lost={standing.overall_league_L} 
+                                    goals_for={standing.overall_league_GF} 
+                                    goals_against={standing.overall_league_GA}
+                                    team_badge={standing.team_badge} 
+                                />    
+                            )}
+                        </tbody>
+                    </table>
+                </div>
+            }
+        </>
     );
 };
 
